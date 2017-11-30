6104eeead1ec4ab91c310f7e5cef793af0848389
neil <neilswalton@gmail.com>
2017-11-29 17:59:56 -0700

    Uploading working cluster SSE and silhouette framework

--- score_funcs.py
+++ score_funcs.py
@@ -1,9 +1,99 @@
+import numpy as np
 
-
-def score_1(cluster):
-    return 1
+def score_1(clusters):
+	return 1
 score_1.name = "Score 1"
 
-def score_2(cluster):
-    return 2
+def score_2(clusters):
+	return 2
 score_2.name= "Score 2"
+
+def cluster_sse(clusters):
+	''' The cluster SSE evaluation method. This method calculates the summed
+		squared error, measured by the distance between the centroid of each
+		cluster and each of the points within that cluster. '''
+
+	total_sse = 0
+	centroids = _calculate_centroids(clusters)
+
+	for i, cluster in enumerate(clusters):
+		
+		total_sse += _single_cluster_sse(cluster, centroids[i])
+
+	return total_sse
+cluster_sse.name = "Cluster SSE"
+
+def _single_cluster_sse(cluster, centroid):
+	''' Calculate the SSE for a single cluster. '''
+
+	sse = 0
+
+	for point in cluster:
+
+		#Calculate dist(cluster, point)^2
+		squared_error = (np.linalg.norm(centroid - point))**2
+		sse += squared_error
+
+	return sse
+
+def silhouette_coefficient(clusters):
+	''' The silhouette coefficient evaluation method. This method combines
+		cohesion and separation by calculating the average distance from the
+		centroid of each cluster to the data points within that cluster as
+		well as the minimum separation of each cluster. These are combined to 
+		a single measure. '''
+
+	s_c = 0
+	centroids = _calculate_centroids(cluster)
+
+	for i, cluster in enumerate(clusters):
+
+		avg_dist = _average_distance(cluster, centroids[i])
+		separation = _minimum_separation(centroids, i)
+
+	return s_c
+silhouette_coefficient.name = "Silhouette Coefficient"
+
+def _average_distance(cluster, centroid):
+	''' Given a cluster and that cluster's centroid, calculate the average
+		distance between the centroid and all points in the cluster '''
+
+def _minimum_separation(centroids, i):
+	''' For centroid i, find the distance to the nearest centroid '''
+
+
+def _calculate_centroids(clusters):
+	''' Given a clustering, compute the centroids for each cluster. '''
+	
+	centroids = []
+
+	for cluster in clusters:
+
+		centroid = np.mean(cluster, axis=0)
+		centroids.append(centroid)
+
+	return centroids
+
+
+
+if __name__ == '__main__':
+
+	cluster1 = [np.array([1, 2, 3]), np.array([2, 4, 3])]
+	cluster2 = [np.array([.1, .2, .3]), np.array([.2, .4, .3])]
+	cluster3 = [np.array([11, 21, 23]), np.array([21, 14, 26]), np.array([17, 22, 16])]
+
+	clusters = [cluster1, cluster2, cluster3]
+
+	print ("Clusters: ")
+	for c in clusters:
+		print (c)
+	print ("")
+	
+	centroids = _calculate_centroids(clusters)
+	print ("Centroids: ")
+	for centroid in centroids:
+		print (str(centroid))
+
+	error = cluster_sse(clusters)
+	print ("Error:" + str(error))
+

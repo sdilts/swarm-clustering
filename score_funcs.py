67e9ed8c52c774f13448a7d91996c28728d83e1c
neil <neilswalton@gmail.com>
2017-11-29 18:21:05 -0700

    Silhouette semi-working. Unsure how to sum the coefficients

--- score_funcs.py
+++ score_funcs.py
@@ -44,12 +44,23 @@ def silhouette_coefficient(clusters):
 		a single measure. '''
 
 	s_c = 0
-	centroids = _calculate_centroids(cluster)
+	centroids = _calculate_centroids(clusters)
 
 	for i, cluster in enumerate(clusters):
 
 		avg_dist = _average_distance(cluster, centroids[i])
 		separation = _minimum_separation(centroids, i)
+
+		print ("AVG dist: " + str(avg_dist))
+		print ("Separation: " + str(separation))
+
+		if avg_dist < separation:
+			coefficient = (separation - avg_dist) / separation
+		else:
+			coefficient = (separation - avg_dist) / separation
+
+		s_c += coefficient
+
 
 	return s_c
 silhouette_coefficient.name = "Silhouette Coefficient"
@@ -58,9 +69,29 @@ def _average_distance(cluster, centroid):
 	''' Given a cluster and that cluster's centroid, calculate the average
 		distance between the centroid and all points in the cluster '''
 
+	total_distance = 0
+
+	for point in cluster:
+
+		dist = np.linalg.norm(centroid - point)
+		total_distance += dist
+
+	return total_distance/len(cluster) #Average distance
+
 def _minimum_separation(centroids, i):
 	''' For centroid i, find the distance to the nearest centroid '''
 
+	min_dist = float("inf")
+
+	for j, centroid in enumerate(centroids):
+
+		if j != i:	#Don't want to consider the current centroid, distance would be zero
+			dist = np.linalg.norm(centroids[i] - centroid)
+
+			if dist < min_dist:
+				min_dist = dist
+
+	return min_dist
 
 def _calculate_centroids(clusters):
 	''' Given a clustering, compute the centroids for each cluster. '''
@@ -80,7 +111,7 @@ if __name__ == '__main__':
 
 	cluster1 = [np.array([1, 2, 3]), np.array([2, 4, 3])]
 	cluster2 = [np.array([.1, .2, .3]), np.array([.2, .4, .3])]
-	cluster3 = [np.array([11, 21, 23]), np.array([21, 14, 26]), np.array([17, 22, 16])]
+	cluster3 = [np.array([11, 21, 23]), np.array([21, 14, 2600]), np.array([17, 22, 16])]
 
 	clusters = [cluster1, cluster2, cluster3]
 
@@ -94,6 +125,5 @@ if __name__ == '__main__':
 	for centroid in centroids:
 		print (str(centroid))
 
-	error = cluster_sse(clusters)
+	error = silhouette_coefficient(clusters)
 	print ("Error:" + str(error))
-

95a91af24ee4ff23d2c940342ba2efb2a317e961
neil <neilswalton@gmail.com>
2017-12-11 02:05:32 -0700

    Cleaned up comments for ACO.py

--- ACO.py
+++ ACO.py
@@ -23,16 +23,14 @@ def ACO(dataset, iterations, num_clusters, num_ants, beta, prob_cutoff, num_elit
 
     for iteration in range(iterations):
 
-        print("-------------------------------------------------------------------")
-        print ("Iteration: " + str(iteration + 1))
-
+        #Loop through all data points and have all ants cluster each data point
         for point_number in range(len(dataset)):
 
             for i, ant in enumerate(ants):
                         
-                ant.update_beliefs() #Could potentially parallelize this step
+                ant.update_beliefs()
 
-        #After all data points have been classified for all ants, rank the ants by objective function
+        #After all data points have been assigned to a cluster for all ants, rank the ants by objective function
         rank_info = _rank_ants(ants)
         ants = [ranked_ant[0] for ranked_ant in rank_info.ants_and_scores]
         
@@ -42,8 +40,8 @@ def ACO(dataset, iterations, num_clusters, num_ants, beta, prob_cutoff, num_elit
 
         iteration_best_score = rank_info.best_score
         iteration_best_clustering = rank_info.best_clustering
-        #results.append(iteration_best_score)
 
+        #If we found a better clustering this iteration, update the global best
         if iteration_best_score < best_score:
             best_score = iteration_best_score
             best_clustering = iteration_best_clustering
@@ -51,8 +49,8 @@ def ACO(dataset, iterations, num_clusters, num_ants, beta, prob_cutoff, num_elit
         #Reset the ants' memory lists
         _reset_ants(ants)
 
+        #Score the best cluster, and append it to the list of values to be returned
         result = Analyze.analyze_clusters(best_clustering, score_funcs)
-        # print("In CL result: %s" % str(result))
         results.append(result)
 
     return results
@@ -67,7 +65,7 @@ def _initialize_pheromones(dataset, num_clusters):
     num_instances = len(dataset)
 
     for i in range(num_clusters):
-        cluster_pheromones = np.random.uniform(0, 0.2, size=num_instances)
+        cluster_pheromones = np.random.uniform(0.7, 0.8, size=num_instances) #Initial values sourced from Kao & Cheng, 2006
         pheromone_matrix.append(cluster_pheromones)
 
     return pheromone_matrix
@@ -99,12 +97,13 @@ def _rank_ants(ants):
     ranked_ants = []
     ant_rank = collections.namedtuple('ant_rank', ['best_score', 'best_clustering', 'ants_and_scores'])
 
+    #For every ant, produce a clustering solution, evaluate that clustering, and append the ant/score to a list
     for ant in ants:
         clusters = ant.get_clustering()
         sse = score_funcs.cluster_sse(clusters)
         ranked_ants.append((ant, sse))
 
-        #If best score see
+        #If best score seen this iteration, keep track of the score and the clustering
         if sse < iteration_best_score:
             iteration_best_score = sse
             iteration_best_clustering = clusters
@@ -152,29 +151,3 @@ def _print_ant_info(ants):
         print ("weights: " + str(ant.weights))
         print ("centroids: " + str(ant.centroids))
         print ("")
-
-
-if __name__ == '__main__':
-
-    scores = [score_funcs.cluster_sse]
-    #data = [[15, 26, 13], [.023, .222, .999], [13, 22, 16], [.015, .322, .897]]
-    #ACO(data, iterations = 15, num_clusters = 2, num_ants = 2, beta = 0.5, prob_cutoff = 0.75, num_elite_ants = 1)
-
-    file = open("iris.data", "r")
-    data_lines = file.readlines()
-    data = []
-    for line in data_lines:
-        
-        data_line = line.split(",")[0:4]
-        if len(data_line) > 2:
-            data.append(data_line)
-
-    for i in range(len(data)):
-        for j in range(len(data[i])):
-            data[i][j] = float(data[i][j])
-
-
-    results = ACO(data, iterations = 10, num_clusters = 3, num_ants = 8, beta = 0.5, 
-        prob_cutoff = 0.75, num_elite_ants = 3, decay_rate = .75, q = 1, score_funcs = scores)
-    for result in results:
-        print(result)    
